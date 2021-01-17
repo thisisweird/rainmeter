488ad704765abbcf0fe1fda7a7b5e9cc5c5fe7d2
thisisweird <71295916+thisisweird@users.noreply.github.com>
2021-01-17 13:26:49 -0500

    idk

--- Version.h
+++ Version.h
@@ -1,11 +1,11 @@
 #pragma once
-#define FILEVER 4,4,0,3412
+#define FILEVER 4,4,0,3413
 #define PRODUCTVER FILEVER
-#define STRFILEVER "4.4.0.3412"
+#define STRFILEVER "4.4.0.3413"
 #define STRPRODUCTVER STRFILEVER
 #define APPVERSION L"4.4.0"
 #define RAINMETER_VERSION ((4 * 1000000) + (4 * 1000) + 0)
-#define COMMIT_HASH L"d7e3df68"
-#define BUILD_TIME L" Sun-7/-02 13:19:11"
-const int revision_number = 3412;
+#define COMMIT_HASH L"75b888ff"
+#define BUILD_TIME L" Sun-7/-02 13:23:56"
+const int revision_number = 3413;
 const bool revision_beta = true;
