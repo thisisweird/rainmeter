75b888ff737a4d5b07742455a32ac2abf9b6cc1e
thisisweird <71295916+thisisweird@users.noreply.github.com>
2021-01-17 13:22:41 -0500

    I think i tried.
    idk

--- Version.h
+++ Version.h
@@ -1,9 +1,11 @@
 #pragma once
-#define FILEVER 4,4,0,3321
+#define FILEVER 4,4,0,3412
 #define PRODUCTVER FILEVER
-#define STRFILEVER "4.4.0.3321"
+#define STRFILEVER "4.4.0.3412"
 #define STRPRODUCTVER STRFILEVER
 #define APPVERSION L"4.4.0"
 #define RAINMETER_VERSION ((4 * 1000000) + (4 * 1000) + 0)
-const int revision_number = 3321;
+#define COMMIT_HASH L"d7e3df68"
+#define BUILD_TIME L" Sun-7/-02 13:19:11"
+const int revision_number = 3412;
 const bool revision_beta = true;
